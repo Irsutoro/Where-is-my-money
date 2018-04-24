5e80de81fa639deea0425d44621c5bf737fe0eef
Oskar Rutkowski <oski0927@gmail.com>
2018-04-24 19:31:38 +0200

    Poprawa strony błędu

--- src/frontend/src/components/general/NotFound.js
+++ src/frontend/src/components/general/NotFound.js
@@ -3,18 +3,19 @@ import { Grid, Header, Image } from 'semantic-ui-react';
 
 import logo from '../../resources/images/logo.svg'
 
+
 export default class NotFound extends Component {
     render() {
         return (
-            <Grid stackable columns={3} className='error-grid'>
-            <Grid.Column width={6}>
+            <Grid stackable columns={3} textAlign='center' className='error-page'>
+            <Grid.Column width={6} >
             </Grid.Column>
-                <Grid.Column centered width={3} textAlign='centered'>
-                    <Header as='h1' color='brown' >Błąd 404</Header>
+                <Grid.Column  centered width={3} textAlign='centered'>
+                    <Header as='h1' color='olive' >Błąd 404</Header>
                     <br/>
-                    <Image src={logo} size='medium'/>
+                    <Image src={logo} size='large'/>
                     <br/>
-                    <Header as='h1' color='brown'>Nie znaleziono strony?!</Header>
+                    <Header as='h1' color='olive '>Nie znaleziono strony?!</Header>
                 </Grid.Column>
             <Grid.Column width={6}>
             </Grid.Column>    
