4bf6582047c013783a7efba92bc07f2e1a26d1a0
Oskar Rutkowski <oski0927@gmail.com>
2018-05-09 20:26:30 +0200

    Dodanie funkcjonalności modala

--- src/frontend/src/components/MainPage/MainPage.js
+++ src/frontend/src/components/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Sidebar, SidebarPushable, SidebarPusher, MenuItem, Button, Menu, Icon } from 'semantic-ui-react';
+import { Segment, Sidebar, SidebarPushable, SidebarPusher, MenuItem, Button, Menu, Icon, Header, Modal, Form, Input } from 'semantic-ui-react';
 
 import './MainPage.css'
 import vid from '../../resources/video/dollar.mp4'
@@ -7,9 +7,13 @@ export default class MainPage extends Component {
     constructor(props) {
         super(props)
         this.state = { 
-            visible: false 
+            visible: false, 
+            isParentOpen: false,
+            isChildOpen: false
         };
         this.toggleVisibility = this.toggleVisibility.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleFocus=this.handleFocus.bind(this);
     }
 
     
@@ -17,7 +21,16 @@ export default class MainPage extends Component {
     toggleVisibility(){
         this.setState({ visible: !this.state.visible });
     }
-
+    handleClick(){
+        this.setState({
+            isParentOpen: !this.state.isParentOpen
+        });
+    }
+    handleFocus(){
+        this.setState({
+            isChildOpen: true
+        });
+    }
     
 
     render() {
@@ -45,7 +58,28 @@ export default class MainPage extends Component {
                         </Menu.Item>
                         <Menu.Item className="add-new-subaccount">
                         <Icon name='add circle' />
-                        <a href='/'>Dodaj subkonto</a>
+                        <a onClick={this.handleClick}> Dodaj subkonto</a>
+                        <Modal open={this.state.isParentOpen}>
+                            <Modal.Header>Dodaj subkonto</Modal.Header>
+                            <Modal.Content >
+                                <Form>
+                                <Input onFocus={this.handleFocus} list='languages' size='big' placeholder='Wybierz nazwę nowego subkonta' />
+                                    <datalist id='languages'>
+                                        <option value='Rozrywka' />
+                                        <option value='Zdrowie' />
+                                        <option value='Praca' />
+                                        <option value='Zakupy' />
+                                    </datalist>
+                                </Form>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button onClick={this.handleClick} negative className='modal-btn'>
+                                No
+                                </Button>
+                                <Button onClick={this.handleClick} positive icon='checkmark' className='modal-btn' labelPosition='right' content='Yes' />
+                            </Modal.Actions>
+                        </Modal>
+
                         </Menu.Item>
                     </Sidebar>
                     <Sidebar.Pusher>
@@ -54,20 +88,18 @@ export default class MainPage extends Component {
                     <header className="v-header container">
                     
                         <div className="fullscreen-video-wrap">
-                           <video src={vid} autoPlay="true" loop="true"/>
+                           <video autoPlay loop src={vid} muted/>
                         </div>
                         <div className="header-overlay"></div>
                         <div className="header-content text-md-center">
     
                             <h1>Witaj w aplikacji<br/> Where's my money?!</h1>
-                            <p>Już teraz stwórz swoje nowe subkonto <br/>i zacznij zarządzać swoimi finansami!</p>
-                            <Button >Utwórz subkonto</Button>
+                            <p>Już teraz stwórz swoje nowe<br/> subkonto i zacznij zarządzać <br/>swoimi finansami!</p>
+                            <Button onClick={this.handleClick}>Utwórz subkonto</Button>
                             <br/>
                             <Button onClick={this.toggleVisibility} className="floating-button" >Menu</Button>
                         </div>
                     </header>
-                        
-                    
                     </Sidebar.Pusher>
                 </Sidebar.Pushable>
            
